7e6d8df5ea7562685854cc5416722a03b7372038
Ameet <amitvish27@gmail.com>
2017-11-22 18:32:24 -0600

    separating cluster in its class

---
+++ cluster.py
@@ -0,0 +1,53 @@
+import math
+
+class Point(object):
+    '''
+    A point object with coords(x, y) and n dimension
+    '''
+    def __init__(self, coords):
+        self.coords = coords
+        self.n = len(coords)
+
+    def __repr__(self):
+        return str(self.coords)
+
+class Cluster(object):
+    '''
+    Cluster of points with a centroid
+    '''
+    def __init__(self, points):
+        if len(points)==0:
+            raise Exception("ERROR: empty cluster")
+        self.points = points
+        self.n = points[0].n
+        self.centroid = self.calc_centroid()
+    
+    def __repr__(self):
+        return str(self.points)
+
+    def calc_centroid(self):
+        '''
+        '''
+        count = len(self.points)
+        coords = [p.coords for p in self.points]
+        centroid = Point([math.fsum(lst)/count for lst in zip(*coords)])
+        return centroid
+
+    def update(self, points):
+        '''
+        update current centroid with the new centroid
+        '''
+        curr_centroid = self.centroid
+        self.points = points
+        self.centroid = self.calc_centroid()
+        return distance(curr_centroid, self.centroid)
+
+def distance(a, b):
+    '''
+    Caculate the Euclidean distance between points a and b as
+    distance(x,y) = sqrt(sum(pow(x-y,2)))
+    '''
+    diff = 0.0
+    for i in range(a.n):
+        diff += pow((a.coords[i]-b.coords[i]),2)
+    return math.sqrt(diff)
--- kmeans.py
+++ kmeans.py
@@ -1,90 +1,56 @@
-# import
-from copy import deepcopy
+#imports 
+import math
 
-import matplotlib.pyplot as plt, mpld3
 import numpy as np
 import pandas as pd
+import plotly
+from plotly.graph_objs import Layout, Scatter, Scatter3d
 
-#plt.rcParams['figure.figsize'] = (16, 9)
-plt.style.use('ggplot')
+from cluster import Cluster, Point, distance
+
+'''
+This program is an implementation of K-means clustering algorithm
+which reads the restaurants datasets to find the best location
+'''
+#global variables for configuration
+dataset_file = "./restaurants-data.csv"
+num_clusters = 3
+tolerance = 0.2
 
 
-def dist(a, b, ax=1):
-    return np.linalg.norm(a - b, axis=ax)
 
-# Main method
+def make_points(list_x, list_y):
+    '''
+    create point objects from 2 lists and return list with coordinates 
+    '''
+    p = []
+    for x,y in zip(list_x, list_y) :
+        p.append(Point([x, y]))
+    return p
+    
+
+def read_from_csv(col1, col2, col3):
+    '''
+    returns a list of points containing col1 as labels, and col2,col3
+    as coordinate (x,y) from the dataset 
+    '''
+    csv_data = pd.read_csv(dataset_file)
+    csv_data.head()
+    list1 = csv_data[col1].values
+    list2 = csv_data[col2].values
+    list3 = csv_data[col3].values
+    #make points list
+    p = make_points(list2, list3)
+    #print(p)
+    return list1, p
 
 
 def main():
-    csvdata = pd.read_csv(r"./dataset.csv")
-    print(csvdata.shape)
-    csvdata.head()
-    lbl = csvdata['lab'].values
-    f1 = csvdata['x'].values
-    f2 = csvdata['y'].values
-    # printing the variables
-    #print("f1",f1)
-    #print("f2",f2)
-    #print("lbl", lbl)
-    X = np.array(list(zip(f1, f2)))
-    #plt.scatter(f1, f2, c='black', s=5)
-    # Number of clusters
-    k = 3
-    np.random.seed()
-    # X coordinates of random centroids
-    C_x = np.random.randint(0, np.max(X)-50, size=k)
-    # Y coordinates of random centroids
-    C_y = np.random.randint(0, np.max(X)-50, size=k)
-    C = np.array(list(zip(C_x, C_y)), dtype=np.float32)
-    #print("C" , C)
-    # Plotting along with the Centroids
-    #plt.scatter(f1, f2, c='#050505', s=7)
-    #plt.scatter(C_x, C_y, marker='*', s=50, c='g')
+    print("in main")
+    labels, points = read_from_csv("premise_name", "latitude", "longitude")
+    print(points)
+    print(labels)
     
-    # To store the value of centroids when it updates
-    C_old = np.zeros(C.shape)
-    #print("C_old", C_old)
-    # Cluster Lables(0, 1, 2)
-    clusters = np.zeros(len(X))
-    #print("clusters", clusters)
-    # Error func. - Distance between new centroids and old centroids
-    error = dist(C, C_old, None)
-    #print("error",error)
-    # Loop will run till the error becomes zero
-    while error != 0:
-        # Assigning each value to its closest cluster
-        for i in range(len(X)):
-            distances = dist(X[i], C)
-            cluster = np.argmin(distances)
-            clusters[i] = cluster
-        # Storing the old centroid values
-        C_old = deepcopy(C)
-        #print("C_old after deepcopy",C_old)
-        # Finding the new centroids by taking the average value
-        for i in range(k):
-            points = [X[j] for j in range(len(X)) if clusters[j] == i]
-            C[i] = np.mean(points, axis=0)
-        error = dist(C, C_old, None)
-    
-    colors = ['r', 'g', 'b', 'y', 'c', 'm']
-    fig, ax = plt.subplots(subplot_kw=dict(facecolor='#EEEEEE'))
-    elements = []
-
-    for i in range(k):
-            points = np.array([X[j] for j in range(len(X)) if clusters[j] == i])
-            element = ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
-            elements.append([element])
-    
-    element = ax.scatter(C[:, 0], C[:, 1], marker='*', s=50, c='#050505')
-    elements.append([element])
-    #ax.grid(color='white', linestyle='solid')
-    ax.set_title("Scatter Plot for clusters", size=20)
-    new_labels = ['pnt {0}'.format(i) for i in lbl]
-    print(new_labels)
-    tooltip = mpld3.plugins.PointLabelTooltip(elements, labels=new_labels)
-    mpld3.plugins.connect(fig, tooltip)
-
-    mpld3.show()
 
 
 if __name__ == "__main__":
